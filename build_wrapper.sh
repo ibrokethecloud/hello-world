cc6c79c5c29c65a01b7e93baa281401431cc7680
Gaurav Mehta <gmehta3@aol.com>
2020-04-15 18:52:01 +1000

    Moved to dind

--- build_wrapper.sh
+++ build_wrapper.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 echo "Build wrapper to orchestrate build"
-apk add --no-cache curl
-echo ">>> Download img tool >>>"
-curl -Lo ${TMP}/img  https://github.com/genuinetools/img/releases/download/v0.5.7/img-linux-amd64
-chmod +x ${TMP}/img
 
-echo ">>> Download helm binary >>>"
-curl -Lo ${TMP}/helm.tar.gz https://get.helm.sh/helm-v3.0.0-rc.4-linux-amd64.tar.gz
-tar -zxvf ${TMP}/helm.tar.gz
 
 echo ">>> Perform Container Build and Push >>>"
-${TMP}/img build -t gmehta3/hello-world:${GO_PIPELINE_COUNTER:0:7} .
-${TMP}/img login -u ${DOCKER_LOGIN} -p ${DOCKER_PASSWORD}
-${TMP}/img push gmehta3/hello-world:${GO_PIPELINE_COUNTER:0:7}
+docker build -t gmehta3/hello-world:${GO_PIPELINE_COUNTER:0:7} .
+docker login -u ${DOCKER_LOGIN} -p ${DOCKER_PASSWORD}
+docker push gmehta3/hello-world:${GO_PIPELINE_COUNTER:0:7}
